7aa6a92161187323a180f084ce12153c5947aeba
Ciubotaru-Bordeianu_Andrei <canapoca@yahoo.com>
2017-05-02 12:55:22 +0200

    added evidence of which room player is in. added evidence of number of enemies in room. working on adding walls.

---
+++ QUATER 1.4 project/Assets/Scripts/Enemy/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    private GameObject CurrentRoom;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void GiveRoom(GameObject room)
+    {
+        CurrentRoom = room;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDestroy()
+    {
+        CurrentRoom.GetComponent<RoomScript>().RemoveEnemy(gameObject);
+    }
+}
--- QUATER 1.4 project/Assets/Scripts/Level/RoomScript.cs
+++ QUATER 1.4 project/Assets/Scripts/Level/RoomScript.cs
@@ -3,14 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomScript : MonoBehaviour {
-
+    public GameObject WallBrush;
+    private List<GameObject> _walls;
+    private GameObject _player;
+    private List<GameObject> _enemies;
+    private bool _hasWalls;
 	// Use this for initialization
 	void Start () {
-		
+        _enemies = new List<GameObject>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag=="Player")
+        {
+            //maybe remove if not necessary next line
+            collision.gameObject.GetComponent<PlayerScript>().CurrentRoom = gameObject;
+            _player = collision.gameObject;
+        }
+        if (collision.transform.tag == "Enemy")
+        {
+            _enemies.Add(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            _player = null;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if(_player!=null && _enemies.Count>0)
+        {
+            CreateWalls();
+        }
+        else if(_enemies.Count==0 && _hasWalls)
+        {
+            RemoveWalls();
+        }
 	}
+
+    private void CreateWalls()
+    {
+        _hasWalls = true;
+    }
+
+    private void RemoveWalls()
+    {
+        _hasWalls = false;
+    }
+
+    public void RemoveEnemy(GameObject enemy)
+    {
+        _enemies.Remove(enemy);
+    }
 }
