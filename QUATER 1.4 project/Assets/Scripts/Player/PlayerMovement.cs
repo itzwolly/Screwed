4f1f974e460527851ac5eb7a332ad9e73539be73
Ciubotaru-Bordeianu_Andrei <canapoca@yahoo.com>
2017-05-02 19:28:16 +0200

    stuff together still fix rotation when walking into wall

--- QUATER 1.4 project/Assets/Scripts/Player/PlayerMovement.cs
+++ QUATER 1.4 project/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public float Speed;
+    private Vector3 _speed;
     [Range(0,1)] 
     public float Sensitivity;
     private Vector3 _velocity;
@@ -13,25 +14,31 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        _speed = new Vector3(Speed, Speed, Speed);
     }
 	
 	// Update is called once per frame
 	private void Update () {
         transform.Rotate(0, Input.GetAxis("Mouse X")*(1+Sensitivity), 0);
+
         if (Input.GetKey(KeyCode.W)) _velocity.z=Speed;
         else if (Input.GetKey(KeyCode.S)) _velocity.z=-Speed;
         if (Input.GetKey(KeyCode.A)) _velocity.x=-Speed;
         else if (Input.GetKey(KeyCode.D)) _velocity.x=Speed;
 
-        _velocity = transform.rotation * _velocity;
+        if(transform.GetComponent<Rigidbody>().velocity.magnitude>0.1f)
+            _velocity = transform.rotation * _velocity;
 
+        Debug.Log(transform.GetComponent<Rigidbody>().velocity.magnitude);
         if(Input.GetKeyDown(KeyCode.E))
         {
-            GameObject _currentRoom = gameObject.GetComponent<PlayerScript>().CurrentRoom;
-            foreach (Transform exit in _currentRoom.GetComponent<ExitList>().Exits)
+            GameObject _currentRoom = null;
+            _currentRoom = gameObject.GetComponent<PlayerScript>().CurrentRoom;
+            if (_currentRoom.GetComponent<ExitList>() != null)
+            for(int i=0;i< _currentRoom.GetComponent<ExitList>().Exits.Count;i++)
             {
-                if((gameObject.transform.position-exit.position).magnitude<=3)
+                Transform exit = _currentRoom.GetComponent<ExitList>().Exits[i];
+                if ((gameObject.transform.position-exit.position).magnitude<=3)
                 {
                     if(_currentRoom.GetComponent<RoomScript>().HasEnemies())
                     {
@@ -53,6 +60,8 @@ public class PlayerMovement : MonoBehaviour {
     {
         
         gameObject.GetComponent<Rigidbody>().velocity = (_velocity);
+        if (_velocity.magnitude > Speed)
+            _velocity.normalized.Scale(_speed);
         _velocity = Vector3.zero;
         gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
     }
