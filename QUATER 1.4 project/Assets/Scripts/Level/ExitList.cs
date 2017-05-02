ede93984e5129bea539242a8d98b9f343d4574da
Ciubotaru-Bordeianu_Andrei <canapoca@yahoo.com>
2017-05-02 12:01:23 +0200

    no message

---
+++ QUATER 1.4 project/Assets/Scripts/Level/ExitList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitList : MonoBehaviour {
+
+    [Header("These lists do not need to be filled in unity")]
+    public List<Transform> Exits;
+	// Use this for initialization
+	void Start () {
+        Exits = new List<Transform>();
+        foreach(Transform obj in gameObject.transform)
+        {
+            if(obj.tag=="RoomExit")
+            {
+                Exits.Add(obj);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
