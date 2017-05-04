581bcf9cb10a544fac8511b30ccb101ddf16fbe1
Ciubotaru-Bordeianu_Andrei <canapoca@yahoo.com>
2017-05-04 15:44:32 +0200

    player movement still iffy, to merge with wollys version. enemies follow, do not go to last position yet.

--- QUATER 1.4 project/Assets/Scripts/Enemy/EnemyMovement.cs
+++ QUATER 1.4 project/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,18 +6,24 @@ public class EnemyMovement : MonoBehaviour {
 
     enum State
     {
-        Walk,
-        Attack
+        none,
+        walk,
+        stop,
+        shoot,
+        knife
     };
+    private State _state;
     // change below to public if not NavMesh
     public GameObject target;
     public float Speed;
-    //UnityEngine.AI.NavMeshAgent navigator;
+    UnityEngine.AI.NavMeshAgent navigator;
     public int Wait;
     public float MeleeDistance;
     public float RangeDistance;
+    public float NoticeDistance;
     private int _wait=0;
     private float _distanceToTarget;
+    private bool _inVision;
 
     Vector3 _speed;
     Vector3 rayDirection;
@@ -26,64 +32,50 @@ public class EnemyMovement : MonoBehaviour {
     void Start ()
     {
         _speed = new Vector3(Speed*3, Speed*3, Speed*3);
-        //navigator = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        navigator = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (target != null)
         {
-            RaycastHit hit = new RaycastHit();
             _distanceToTarget = (target.transform.position - gameObject.transform.position).magnitude;
             if (_distanceToTarget<RangeDistance)
             {
                 Utils.ChangeGameObjectColor(gameObject, Color.white);
-
-                if (_wait <= 0)
-                {
-                    if (_distanceToTarget<MeleeDistance)
-                    {
-                        Utils.ChangeGameObjectColor(gameObject,Color.blue);
-                        Debug.Log("about to melee");
-                    }
-                    else
-                    {
-                        if(Physics.Linecast(transform.position,target.transform.position,out hit))
-                        {
-                            if(hit.transform.tag=="Player")
-                            {
-                                Utils.ChangeGameObjectColor(gameObject, Color.red);
-                                Debug.Log("about to shoot");
-                            }
-                            else
-                                Debug.Log("no vision");
-                        }
-                        else
-                        {
-                            //if (hit.transform.tag == "Player")
-                            //{
-                                Utils.ChangeGameObjectColor(gameObject, Color.red);
-                                Debug.Log("about to shoot");
-                            //}
-                            //else
-                            //    Debug.Log("no vision");
-                        }
-                    }
-                    _wait = Wait;
-                }
-                _wait--;
-                MoveTowardTarget();
+                CheckVision();
             }
             else
             {
-                MoveTowardTarget();
-                //navigator.SetDestination(target.transform.position);
+                _inVision = false;
+            }
+
+            if(_inVision)
+            {
+                EnemyAttack();
+            }
+            else
+            {
+                Patrol();
             }
         }
 	}
 
-    private void MoveTowardTarget()
+    
+
+    private void Patrol()
     {
+        _state = State.walk;
+        Debug.Log("Patrolling");
+    }
+
+    private void SetTragetDestinationTpPlayer()
+    {
+        Debug.Log("setting target location to go to is = "+ target.transform.position);
+
+        _state = State.walk;
+        navigator.SetDestination(target.transform.position);
+        /**
         if (gameObject.GetComponent<Rigidbody>().velocity.magnitude < Speed)
         {
             moveDirection = target.transform.position - transform.position;
@@ -91,6 +83,72 @@ public class EnemyMovement : MonoBehaviour {
             moveDirection.Scale(_speed);
             GetComponent<Rigidbody>().AddForce(moveDirection);
         }
+        /**/
+    }
+
+    private void CheckVision()
+    {
+        _inVision = true;
+        if (_distanceToTarget < MeleeDistance)
+        {
+            Utils.ChangeGameObjectColor(gameObject, Color.blue);
+            StopMovement();
+            //Debug.Log("In melee range");
+            _state = State.knife;
+            //Debug.Log("About to attack (Melee)");
+            return;
+        }
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Linecast(transform.position, target.transform.position, out hit))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                Utils.ChangeGameObjectColor(gameObject, Color.red);
+                _state = State.shoot;
+            }
+            else
+            {
+                _inVision = false;
+                _state = State.none;
+                Debug.Log("no vision");
+            }
+        }
+        else
+        {
+            //if (hit.transform.tag == "Player")
+            //{
+            Utils.ChangeGameObjectColor(gameObject, Color.red);
+            _state = State.shoot;
+            //}
+            //else
+            //    Debug.Log("no vision");
+        }
+    }
+
+    private void StopMovement()
+    {
+        _state = State.none;
+        navigator.SetDestination(transform.position);
+    }
+
+    private void EnemyAttack()
+    {
+        if (_wait <= 0)
+        {
+            if (_state == State.shoot)
+            {
+                Debug.Log("shoot shoot");
+                SetTragetDestinationTpPlayer();
+            }
+
+            if (_state == State.knife)
+            {
+                Debug.Log("Knify knify");
+                StopMovement();
+            }
+            _wait = Wait;
+        }
+        _wait--;
     }
 
     public void GiveTarget(GameObject ptarget)
