8bdc357380eaa535a2ce0fa465e64f780645572d
Wolly <wolly.m.b@gmail.com>
2017-05-12 13:32:30 +0200

    Added pickup

---
+++ QUATER 1.4 project/Assets/Scripts/Player/Pickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(WeaponHandler))]
+public class Pickup : MonoBehaviour {
+    [SerializeField] private GameObject _gunPickup;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other == _gunPickup.GetComponentInChildren<Collider>()) {
+            GetComponent<WeaponHandler>().HasGun = true;
+            Destroy(_gunPickup);
+        }
+    }
+}
