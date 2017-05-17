8de246b13220c48d16b7825377616eb8fade15ad
Ciubotaru-Bordeianu_Andrei <canapoca@yahoo.com>
2017-05-17 13:09:35 +0200

    fixed jump

--- QUATER 1.4 project/Assets/Scripts/Player/PlayerMovement.cs
+++ QUATER 1.4 project/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour {
     public AudioClip JumpClip;
     private AudioSource audio;
 
+    private bool _jumped;
     public float _speedUnit;
     private Vector3 _speed;
     public int JumpVector;
@@ -16,77 +17,20 @@ public class PlayerMovement : MonoBehaviour {
     public SectionPlacement godController;
     public float _gravity;
     private Vector3 _moveDirection = Vector3.zero;
+    private CharacterController _controller;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
+        _controller = GetComponent<CharacterController>();
         audio = gameObject.GetComponent<AudioSource>();
         _speed = new Vector3(_speedUnit, _speedUnit, _speedUnit);
     }
-	
-	// Update is called once per frame
-	private void Update () {
-        /*
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
-        {
-            _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            _moveDirection = transform.TransformDirection(_moveDirection);
-            _moveDirection *= _speedUnit;
 
-        }
-        
-        _moveDirection.y -= _gravity * Time.deltaTime;
-        controller.Move(_moveDirection * Time.deltaTime);
-        transform.Rotate(0, Input.GetAxis("Mouse X") * (1 + Sensitivity), 0);
-        */
 
-//<<<<<<< HEAD
-        //if (Input.GetKeyDown(KeyCode.E))
-        //{
-        //    GameObject _currentRoom = null;
-        //    _currentRoom = gameObject.GetComponent<PlayerScript>().CurrentRoom;
-        //    if (_currentRoom.GetComponent<ExitList>() != null) {
-        //        for (int i = 0; i < _currentRoom.GetComponent<ExitList>().Exits.Count; i++) {
-        //            Transform exit = _currentRoom.GetComponent<ExitList>().Exits[i];
-        //            if ((gameObject.transform.position - exit.position).magnitude <= 3) {
-        //                if (_currentRoom.GetComponent<RoomScript>().HasEnemies()) {
-        //                    Debug.Log("Room is not cleared");
-        //                } else {
-        //                    godController.AddRooms(exit);
-        //                    _currentRoom.GetComponent<ExitList>().RemoveFirstExits(2);
-        //                    Debug.Log("Im loading next room");
-        //                }
-        //            }
-        //        }
-        //    }
-//        //}
-//=======
-//        //Debug.Log(transform.GetComponent<Rigidbody>().velocity.magnitude);
-//        if (Input.GetKeyDown(KeyCode.E))
-//        {
-//            GameObject _currentRoom = null;
-//            _currentRoom = gameObject.GetComponent<PlayerScript>().CurrentRoom;
-//            if (_currentRoom.GetComponent<ExitList>() != null)
-//            for(int i=0;i< _currentRoom.GetComponent<ExitList>().Exits.Count;i++)
-//            {
-//                Transform exit = _currentRoom.GetComponent<ExitList>().Exits[i];
-//                if ((gameObject.transform.position-exit.position).magnitude<=3)
-//                {
-//                    if(_currentRoom.GetComponent<RoomScript>().HasEnemies())
-//                    {
-//                        Debug.Log("Room is not cleared");
-//                    }
-//                    else
-//                    {
-//                        godController.AddRooms(exit);
-//                        _currentRoom.GetComponent<ExitList>().RemoveFirstExits(2);
-//                        Debug.Log("Im loading next room");
-//                    }
-//                }
-//            }
-//        }
-
-//>>>>>>> c80f05ee8bc3bfa9b15bf5c84b7f295e4a2f4063
+    // Update is called once per frame
+    private void Update () {
+       
     }
 
     private void FixedUpdate()
@@ -104,22 +48,26 @@ public class PlayerMovement : MonoBehaviour {
         /**/
 
         /**/
-        
-
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))//because controller is grounded act weird alternating between true and false
         {
+            _jumped = true;
+        }
+
+        if (_controller.isGrounded)
+        {
+            //Debug.Log("is grounded");
             _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             _moveDirection = transform.TransformDirection(_moveDirection);
             _moveDirection *= _speedUnit;
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (_jumped)
             {
-                Debug.Log("space");
+                //Debug.Log("space");
                 audio.PlayOneShot(JumpClip);
                 _moveDirection.y += JumpVector;
+                _jumped = false;
             }
 
-           // Debug.Log(_moveDirection.magnitude);
+            // Debug.Log(_moveDirection.magnitude);
             if (_moveDirection.magnitude >= 0.5f)
             {
                // Debug.Log(audio.isPlaying);
@@ -132,9 +80,10 @@ public class PlayerMovement : MonoBehaviour {
         }
         else
         {
+            //Debug.Log("not grounded");
             _moveDirection.y -= _gravity * Time.deltaTime;
         }
-        controller.Move(_moveDirection * Time.deltaTime);
+        _controller.Move(_moveDirection * Time.deltaTime);
         transform.Rotate(0, Input.GetAxis("Mouse X") * (1 + Sensitivity), 0);
         //
         gameObject.GetComponent<Rigidbody>().velocity = (_velocity);
