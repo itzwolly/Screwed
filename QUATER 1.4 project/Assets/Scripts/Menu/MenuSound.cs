c4983def5a321f3f7c1cb5107ee4b45b0a715958
Oleksandr Mazur <ruzamxela@gmail.com>
2017-05-18 23:06:37 +0200

    improved main menu

---
+++ QUATER 1.4 project/Assets/Scripts/Menu/MenuSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSound : MonoBehaviour {
+    private AudioSource audio;
+    public AudioClip HoverClip;
+    public AudioClip ClickClip;
+    // Use this for initialization
+    void Start () {
+        audio = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HoverSound()
+    {
+        audio.PlayOneShot(HoverClip);
+    }
+    public void ClickSound()
+    {
+        audio.PlayOneShot(ClickClip);
+    }
+}
